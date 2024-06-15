bcb09f1ddd7c485b27ea2652d2674b72047483cb
sundarkandel <soondar560@gmail.com>
2024-06-16 00:15:44 +1000

    introduction to map function

--- App.js
+++ App.js
@@ -1987,16 +1987,14 @@ const Body =() =>{
       <div className="search"> Search</div>
 
       <div className="res-container">
+ {resList.map((restaurant) => (
+  <RestaurantCard key= {restaurant.info.id} resData = {restaurant}/>
+ ))
 
-         <RestaurantCard resData= {resList [0]} />
-         <RestaurantCard resData= {resList [1]} />
-         <RestaurantCard resData= {resList [2]} />
-         <RestaurantCard resData= {resList [3]} />
-         <RestaurantCard resData= {resList [4]} />
-         <RestaurantCard resData= {resList [5]} />
-         <RestaurantCard resData= {resList [6]} />
-         <RestaurantCard resData= {resList [7]} />
-         <RestaurantCard resData= {resList [8]} /> 
+ }
+
+
+
          
       </div> 
     </div>
